d76e3bc033b3ded7d703145d63249b089edda98b
Michael Putterflam <michael.putterflam@gmail.com>
2020-08-26 23:28:11 -0700

    Small modification to returnAccounts to not return the amount and implemented deposit function

--- Bank.cpp
+++ Bank.cpp
@@ -44,7 +44,7 @@ public:
     void returnAccounts(pqxx::connection &C)
     {
         //Get the bank accounts from database
-        string sql = "SELECT * FROM accounts";
+        string sql = "SELECT id, last_name, first_name FROM accounts";
 
         pqxx::nontransaction N(C);
 
@@ -58,21 +58,11 @@ public:
                 cout << "There are no accounts in the bank." << endl;
             }
 
-            //temp to test read from database
-            //this for loop is from postgreSQL's website
-            // for (pqxx::result::const_iterator i = R.begin(); i != R.end(); ++i)
-            // {
-            //     cout << "ID = " << i[0].as<int>() << endl;
-            //     cout << "Last Name = " << i[1].as<string>() << endl;
-            //     cout << "First Name = " << i[2].as<string>() << endl;
-            //     cout << "Amount = " << i[3].as<string>() << endl;
-            // }
-
             // Considering tabulate for a better table output
-            cout << "ID" << setw(15) << "Last Name" << setw(15) << "First Name" << setw(15) << "Balance" << endl;
+            cout << "ID" << setw(15) << "Last Name" << setw(15) << "First Name" << endl;
             for (pqxx::result::const_iterator i = R.begin(); i != R.end(); ++i)
             {
-                cout << i[0].as<int>() << setw(15) << i[1].as<string>() << setw(15) << i[2].as<string>() << setw(15) << i[3].as<string>() << endl;
+                cout << i[0].as<int>() << setw(15) << i[1].as<string>() << setw(15) << i[2].as<string>() << endl;
             }
         }
         catch (const pqxx::undefined_table &e)
@@ -93,7 +83,7 @@ public:
 
         cout << "Please enter the account last holder's name:" << endl;
         cin >> lastName;
-        cout << "Please enter the account last holder's name:" << endl;
+        cout << "Please enter the account first holder's name:" << endl;
         cin >> firstName;
         cout << "Please enter an initial deposit ammount" << endl;
         cin >> amount;
@@ -110,7 +100,71 @@ public:
 
     void deposit(pqxx::connection &C)
     {
-        cout << "temp" << endl;
+        int id;
+
+        cout << "Please enter the ID of the account you wish to deposit to" << endl;
+        cin >> id;
+
+        string sql = "SELECT * FROM accounts WHERE id=" + to_string(id);
+
+        pqxx::nontransaction N(C);
+
+        try
+        {
+            pqxx::result R(N.exec(sql.c_str()));
+
+            // If there is an empty result, return a message saying so
+            if (R.empty())
+            {
+                cout << "That account does not exist." << endl;
+            }
+
+            // There should only be one record, but not sure how you can read the row from R without itterating through it
+            for (pqxx::result::const_iterator i = R.begin(); i != R.end(); ++i)
+            {
+                string yn;
+
+                do
+                {
+                    cout << "Do you wish to deposit to the account belonging to " << i[2].as<string>() << " " << i[1].as<string>() << " (y/n)?" << endl;
+                    cin >> yn;
+
+                    // Change our read in option to uppercase for comparison
+                    transform(yn.begin(), yn.end(), yn.begin(), ::toupper);
+                } while (yn.compare("Y") != 0 && yn.compare("N") != 0); // I keep thinking this needs to be || but it needs to be &&
+
+                // We should only reach here if the user answered y or n
+                // If confirmed to deposit, read the amount from the database, add to it, and then update the record in the database
+                if (yn.compare("Y") == 0)
+                {
+                    double deposit = 0, amount = i[3].as<double>();
+
+                    cout << "How much do you wish to deposit?" << endl;
+                    cin >> deposit;
+
+                    amount += deposit;
+                    sql = "UPDATE accounts SET amount = " + to_string(amount) + " WHERE id=" + to_string(id);
+                    pqxx::result R(N.exec(sql.c_str()));
+
+                    // We should only be updating one record so it should be safe to break out of the for loop after it's been updated
+                    if (R.affected_rows() == 1)
+                    {
+                        cout << "Update successful" << endl;
+                        break;
+                    }
+                }
+                // If we're not going to deposit, just break out of the for loop
+                else if (yn.compare("N") == 0)
+                {
+                    cout << "Aborting" << endl;
+                    break;
+                }
+            }
+        }
+        catch (const pqxx::undefined_table &e)
+        {
+            std::cerr << e.what() << std::endl;
+        }
     }
 
     void withdraw(pqxx::connection &C)
